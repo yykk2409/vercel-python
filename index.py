e23cedd2c0728215dce232b154d14531f5be9f6e
yykk2409 <maetaka-2022066@edu-g.gsn.ed.jp>
2023-12-30 10:48:07 +0900

    commit

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, render_template, redirect
 from flask_cors import CORS
-#from yt_dlp import YoutubeDL
+from yt_dlp import YoutubeDL
 #import demucs.separate
 
 app = Flask(__name__)
-#CORS(app)
+CORS(app)
 #@app.route("/")
 #def hello_world():
 #   return "<p>Hello, World!</p>"
@@ -13,16 +13,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('main.html')
-"""@app.route('/sss',methods=["POST"])
+@app.route('/sss',methods=["POST"])
 def sss():
 	url = request.form["url"]
 	print(url)
 	ydl_opts = {'format': 'bestaudio','outtmpl': 'sound.mp3'}
 	with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    info = ydl.extract_info(url, download=False)  # ダウンロードはしない（download=False）
-
+    info = ydl.extract_info(url, download=False) 
 # 音声データを変数に格納
 	audio_data = info['url'] 
-	options = [audio_data,"-n","htdemucs", "--two-stems","vocals","--mp3"]
-	demucs.separate.main(options)
-	return render_template('main.html')"""
+"""	options = [audio_data,"-n","htdemucs", "--two-stems","vocals","--mp3"]
+	demucs.separate.main(options)"""
+	return render_template('main.html')
