3d6ef19f566f574148f139074378ec239e034a34
yykk2409 <maetaka-2022066@edu-g.gsn.ed.jp>
2023-12-30 09:24:54 +0900

    commit

--- index.py
+++ index.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect
+#from flask_cors import CORS
+#from yt_dlp import YoutubeDL
+#import demucs.separate
 
 app = Flask(__name__)
-
+#CORS(app)
 @app.route("/")
 def hello_world():
    return "<p>Hello, World!</p>"
+
+
+#@app.route('/')
+#def index():
+#    return render_template('main.html')
+"""@app.route('/sss',methods=["POST"])
+def sss():
+	url = request.form["url"]
+	print(url)
+	ydl_opts = {'format': 'bestaudio','outtmpl': 'sound.mp3'}
+	with YoutubeDL(ydl_opts) as ydl:
+	  ydl.download([url])
+	options = ["sound.mp3","-n","htdemucs", "--two-stems","vocals","--mp3"]
+	demucs.separate.main(options)
+	return render_template('main.html')"""
